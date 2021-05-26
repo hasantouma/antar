6b218f854ce2117bbea9338d4091f1478be9858a
hasantouma <hasantouma@gmail.com>
2021-05-26 01:41:36 -0400

    fixing spacing

--- test/r0_tests.ml
+++ test/r0_tests.ml
@@ -15,10 +15,10 @@ let test_add _ctxt =
   assert_equal "(+ (read) 72)" (pp (`EAdd (`ERead, `EInt 72))) ~msg:"add (3)" ~printer:lowercase_ascii
 
 let test_negate _ctxt =
-  assert_equal "(- 1)"             (pp (`ENegate (`EInt 1)))                  ~msg:"negate (1)" ~printer:lowercase_ascii;
-  assert_equal "(- (read))"        (pp (`ENegate (`ERead)))                   ~msg:"negate (2)" ~printer:lowercase_ascii;
-  assert_equal "(+ (read) (- 72))" (pp (`EAdd (`ERead, `ENegate (`EInt 72)))) ~msg:"negate (3)" ~printer:lowercase_ascii;
-  assert_equal "(- (+ 99 50))" (pp (`ENegate (`EAdd (`EInt 99, `EInt 50))))   ~msg:"negate (4)" ~printer:lowercase_ascii
+  assert_equal "(- 1)"             (pp (`ENegate (`EInt 1)))                      ~msg:"negate (1)" ~printer:lowercase_ascii;
+  assert_equal "(- (read))"        (pp (`ENegate (`ERead)))                       ~msg:"negate (2)" ~printer:lowercase_ascii;
+  assert_equal "(+ (read) (- 72))" (pp (`EAdd (`ERead, `ENegate (`EInt 72))))     ~msg:"negate (3)" ~printer:lowercase_ascii;
+  assert_equal "(- (+ 99 50))"     (pp (`ENegate (`EAdd (`EInt 99, `EInt 50))))   ~msg:"negate (4)" ~printer:lowercase_ascii
 
 let suite =
   "r0_tests" >::: [
