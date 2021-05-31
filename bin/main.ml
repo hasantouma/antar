109dfd1d7a1d99f7ae811dec54a409fef4bfe3a2
hasantouma <hasantouma@gmail.com>
2021-05-30 22:28:27 -0400

    Completed 'Task #6 - Write a function that generates a random R0 program of depth N'

--- bin/main.ml
+++ bin/main.ml
@@ -10,6 +10,12 @@ let () =
   print_endline ("exp5: " ^ (pp e5));
   print_endline ("interp exp5: " ^ (string_of_int (interp e5 (make_read []))))
 
+
+let () =
+  let r3 = randp 3 in
+  print_endline ("randp3: " ^ (pp r3));
+  print_endline ("interp randp3: " ^ (string_of_int (interp r3 (make_read []))))
+
 let () =
   let p = parse_file "src/ex1.ht" in
   print_endline (pp p.e)
--- src/generator/generator.ml
+++ src/generator/generator.ml
@@ -1,5 +1,9 @@
 open Lex_parse.Ast
 
+let () = Random.init (int_of_float (Unix.time ()))
+let random_float_gen : (float -> float) = Random.float
+let random_int_gen : (int -> int) = Random.int
+
 let rec exp2 (n : int) : expr =
   match n with
   | 0 -> `EInt 1
@@ -7,4 +11,20 @@ let rec exp2 (n : int) : expr =
       let left = exp2 (n - 1) in
       let right = exp2 (n - 1) in
       `EAdd(left, right)
+
+let rec randp (n : int) : expr =
+  let random_float = random_float_gen 1.0 in
+  if n = 0 then
+    let random_int = random_int_gen 1024 in
+    match random_float with
+    | f when f < 0.5 -> `ERead
+    | _ -> `EInt random_int
+  else
+    match random_float with
+    | f when f < 0.5 -> `ENegate (randp (n - 1))
+    | _ ->
+        let left = randp (n - 1) in
+        let right = randp (n - 1) in
+        `EAdd (left, right)
+
 
