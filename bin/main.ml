84ec7a60f5177df495ab7e39c5a82f480866a054
hasantouma <hasantouma@gmail.com>
2021-06-06 05:49:12 -0400

    Using the Arg module to handle command line arguments. Now can parse file and generate random programs from command line

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,29 @@
+open Pretty_print
+open Interp
 open Repl
+open Generator
+
+let interp_randp (n : int) : unit =
+  let r = randp n in
+  print_endline (pp_with_indent r);
+  let p = interp r [] in
+  let s = string_of_int p in
+  print_endline s
+
+
+let () =
+  let speclist = [
+    ("-f", Arg.String interp_file, "Parsing file");
+    ("-g", Arg.Int interp_randp, "Generate random program of size n")
+  ] in
+  let usage_msg = "'Antar' programming language" in
+  Arg.parse speclist print_endline usage_msg
+
+
 
 let () =
   let args_len = Array.length Sys.argv in
-  if args_len > 1 then
-    interp_file Sys.argv.(1)
-  else
+  if args_len = 1 then
     begin
       print_endline "Welcome to the 'Antar' REPL";
       repl ()
