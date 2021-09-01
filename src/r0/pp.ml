91caa51938ff31ac11824def5db128166fb9a794
hasantouma <hasantouma@gmail.com>
2021-09-01 17:11:55 -0400

    Completed 'Task #12 - Extend your pretty printer from R0 to R1'

--- src/r0/pp.ml
+++ src/r0/pp.ml
@@ -4,11 +4,16 @@ let pp_open f expr indent =
   | `EInt n -> string_of_int n
   | `ERead -> "(read)"
   | `ENegate e -> "(- " ^ (f e indent) ^ ")"
-  | `EAdd(l, r) ->
+  | `EAdd (l, r) ->
       let left = f l (indent + 3) in
       let spaces = String.make (indent + 3) ' ' in
       let right = f r (indent + 3) in
       "(+ " ^ left ^ "\n" ^ spaces ^ right ^ ")"
+  | `EMult (l, r) ->
+    let left = f l (indent + 3) in
+    let spaces = String.make (indent + 3) ' ' in
+    let right = f r (indent + 3) in
+    "(* " ^ left ^ "\n" ^ spaces ^ right ^ ")"
 
 let rec pp expr indent = pp_open pp expr indent
 
--- src/r1/pp.ml
+++ src/r1/pp.ml
@@ -1,11 +1,11 @@
 
 let pp_open f expr indent =
   match expr with
-  | `EMult(l, r) ->
-      let left = f l (indent + 3) in
-      let spaces = String.make (indent + 3) ' ' in
-      let right = f r (indent + 3) in
-      "(* " ^ left ^ "\n" ^ spaces ^ right ^ ")"
+  | `EVar v -> v
+  | `ELet (v, ve, be) ->
+    let variable_expr = f ve indent in
+    let body_expr = f be indent in
+    "(let ([" ^ v ^ " " ^ variable_expr ^ "]) " ^ body_expr ^ ")"
   | #R0.Ast.r0_open as e -> R0.Pp.pp_open f e indent
 
 let rec pp expr indent = pp_open pp expr indent
