4cf35e478e15c85f11952d420652e3ff62b1717e
hasantouma <hasantouma@gmail.com>
2021-09-09 01:38:21 -0400

    Added 'make graph' to Git pre-commit hook

--- hooks/pre-commit.py
+++ hooks/pre-commit.py
@@ -5,14 +5,24 @@ import subprocess
 print("Running Git pre-commit hook!")
 
 try:
-    cmd = ["dune", "build", "@fmt"]
+    cmd = ["make", "graph"]
+    print("Running 'make graph' to update deps graph.")
     output = subprocess.check_output(cmd)
+
+    cmd = ["dune", "clean"]
+    print("Running 'dune clean' to clean up after 'make graph'.")
+    output = subprocess.check_output(cmd)
+
+    cmd = ["dune", "build", "@fmt"]
+    print("Running 'dune build @fmt' to format all OCaml files..")
+    output = subprocess.check_output(cmd)
+
 except subprocess.CalledProcessError as exc:
     print("Status: FAIL")
     print("Command:", cmd)
     print("Return code:", exc.returncode)
     print("Output:", exc.output)
-    print("Run command: dune promote")
+    print("If cmd is '@fmt' then run command: dune promote")
 
     exit(exc.returncode)
 else:
