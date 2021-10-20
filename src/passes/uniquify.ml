1013fdef43e79ee6b7e2b42d5e67d208ad33da2d
hasantouma <hasantouma@gmail.com>
2021-10-20 03:21:34 -0400

    Completed 'Task #27 - Write a few tests for uniquify that predict its output'

---
+++ src/passes/uniquify.ml
@@ -0,0 +1,1 @@
+let uniquify (expr : R1.Ast.expr) : R1.Ast.expr = expr
---
+++ tests/passes/uniquify_tests.ml
@@ -0,0 +1,39 @@
+open OUnit2
+open Passes.Uniquify
+
+let u1 = `EVar "x"
+
+let u1' = `EVar "x0"
+
+let u2 = `ELet ("a", `ERead, `EVar "a")
+
+let u2' = `ELet ("x0", `ERead, `EVar "x0")
+
+let u3 = `ELet ("a", `ELet ("a", `ENegate (`EInt 3), `EAdd (`EVar "a", `EVar "a")), `EVar "a")
+
+let u3' = `ELet ("x0", `ELet ("x1", `ENegate (`EInt 3), `EAdd (`EVar "x1", `EVar "x1")), `EVar "x0")
+
+let u4 = `ELet ("a", `EInt 4, `ELet ("a", `ENegate (`EInt 3), `EAdd (`EVar "a", `EVar "a")))
+
+let u4' = `ELet ("x0", `EInt 4, `ELet ("x1", `ENegate (`EInt 3), `EAdd (`EVar "x1", `EVar "x1")))
+
+let u5 =
+  `EAdd
+    ( `ELet ("x", `EInt 7, `EVar "x")
+    , `ELet ("x", `EInt 8, `ELet ("x", `EAdd (`EInt 1, `EVar "x"), `EAdd (`EVar "x", `EVar "x"))) )
+
+let u5' =
+  `EAdd
+    ( `ELet ("x0", `EInt 7, `EVar "x0")
+    , `ELet ("x1", `EInt 8, `ELet ("x2", `EAdd (`EInt 1, `EVar "x1"), `EAdd (`EVar "x2", `EVar "x2"))) )
+
+let test_uniquify _ctxt =
+  assert_equal u1' (uniquify u1) ~msg:"u1" ~printer:R1.Pp.pp;
+  assert_equal u2' (uniquify u2) ~msg:"u2" ~printer:R1.Pp.pp;
+  assert_equal u3' (uniquify u3) ~msg:"u3" ~printer:R1.Pp.pp;
+  assert_equal u4' (uniquify u4) ~msg:"u4" ~printer:R1.Pp.pp;
+  assert_equal u5' (uniquify u5) ~msg:"u5" ~printer:R1.Pp.pp
+
+let suite = "passes_tests" >::: [ "uniquify" >:: test_uniquify ]
+
+let _ = run_test_tt_main suite
