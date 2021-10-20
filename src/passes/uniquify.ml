e0ab3be24994ce8e0338ea1b3b5d3521236e84e0
hasantouma <hasantouma@gmail.com>
2021-10-20 04:04:09 -0400

    Completed 'Task #28 - Implement the uniquify pass for R1 programs'

--- src/passes/uniquify.ml
+++ src/passes/uniquify.ml
@@ -1,1 +1,33 @@
-let uniquify (expr : R1.Ast.expr) : R1.Ast.expr = expr
+open R1.Ast
+
+let uniquify (expr : expr) : expr =
+  let fresh_var =
+    let counter = ref 0 in
+    fun () ->
+      let x = "x" ^ string_of_int !counter in
+      counter := !counter + 1;
+      x
+  in
+  let rec uniquify rho expr =
+    match expr with
+    | `EInt _
+    | `ERead ->
+      expr
+    | `ENegate e ->
+      let e' = uniquify rho e in
+      `ENegate e'
+    | `EAdd (l, r) ->
+      let l' = uniquify rho l in
+      let r' = uniquify rho r in
+      `EAdd (l', r')
+    | `EVar x ->
+      let x' = List.assoc x rho in
+      `EVar x'
+    | `ELet (x, xe, be) ->
+      let x' = fresh_var () in
+      let xe' = uniquify rho xe in
+      let rho' = (x, x') :: rho in
+      let be' = uniquify rho' be in
+      `ELet (x', xe', be')
+  in
+  uniquify [] expr
