5bf696470d6d154e1ea7ba51dfbfe5f5f2b8d3b2
hasantouma <hasantouma@gmail.com>
2021-10-24 04:42:13 -0400

    Fixed 'resolve_complex_tests' - 5 & 6 were incorrect

--- tests/passes/resolve_complex_tests.ml
+++ tests/passes/resolve_complex_tests.ml
@@ -19,20 +19,23 @@ let rco4' = `ELet ("x0", `ENegate (`EVar "a"), `ELet ("x1", `ERead, `EAdd (`EVar
 
 let rco5 = `ENegate `ERead
 
-let rco5' = `ELet ("x0", `ELet ("x1", `ERead, `ENegate (`EVar "x1")), `EVar "x0")
+let rco5' = `ELet ("x1", `ERead, `ENegate (`EVar "x1"))
 
 let rco6 = `ENegate (`EAdd (`ERead, `ENegate (`EInt 42)))
 
 let rco6' =
-  `ELet ("x0", `ELet ("x1", `ERead, `ELet ("x2", `ENegate (`EInt 42), `EAdd (`EVar "x1", `EVar "x2"))), `EVar "x0")
+  `ELet
+    ( "x0"
+    , `ELet ("x1", `ERead, `ELet ("x2", `ENegate (`EInt 42), `EAdd (`EVar "x1", `EVar "x2")))
+    , `ENegate (`EVar "x0") )
 
 let test_resolve_complex _ctxt =
-  assert_equal rco1' (resolve_complex rco1) ~msg:"rco1" ~printer:R1.Pp.pp;
-  assert_equal rco2' (resolve_complex rco2) ~msg:"rco2" ~printer:R1.Pp.pp;
-  assert_equal rco3' (resolve_complex rco3) ~msg:"rco3" ~printer:R1.Pp.pp;
-  assert_equal rco4' (resolve_complex rco4) ~msg:"rco4" ~printer:R1.Pp.pp;
-  assert_equal rco5' (resolve_complex rco5) ~msg:"rco5" ~printer:R1.Pp.pp;
-  assert_equal rco6' (resolve_complex rco6) ~msg:"rco6" ~printer:R1.Pp.pp
+  assert_equal rco1' (resolve_complex rco1) ~cmp:TestUtils.alpha_equiv ~msg:"rco1" ~printer:R1.Pp.pp;
+  assert_equal rco2' (resolve_complex rco2) ~cmp:TestUtils.alpha_equiv ~msg:"rco2" ~printer:R1.Pp.pp;
+  assert_equal rco3' (resolve_complex rco3) ~cmp:TestUtils.alpha_equiv ~msg:"rco3" ~printer:R1.Pp.pp;
+  assert_equal rco4' (resolve_complex rco4) ~cmp:TestUtils.alpha_equiv ~msg:"rco4" ~printer:R1.Pp.pp;
+  assert_equal rco5' (resolve_complex rco5) ~cmp:TestUtils.alpha_equiv ~msg:"rco5" ~printer:R1.Pp.pp;
+  assert_equal rco6' (resolve_complex rco6) ~cmp:TestUtils.alpha_equiv ~msg:"rco6" ~printer:R1.Pp.pp
 
 let suite = "resolve_complex_tests" >::: [ "resolve_complex" >:: test_resolve_complex ]
 
