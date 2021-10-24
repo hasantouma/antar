3f56b36522f87f341452a6494d91e8b4bd9ed408
hasantouma <hasantouma@gmail.com>
2021-10-24 03:40:57 -0400

    Moved 'alpha-equiv' to separate helper file

---
+++ tests/passes/testUtils.ml
@@ -0,0 +1,18 @@
+let alpha_equiv e1 e2 : bool =
+  let rec go env e1 e2 =
+    match (e1, e2) with
+    | `EInt n, `EInt m -> n = m
+    | `ERead, `ERead -> true
+    | `EVar x, `EVar y -> (
+      let y_opt = List.assoc_opt x env in
+      match y_opt with
+      | Some y' -> y = y'
+      | None -> x = y)
+    | `ENegate e1', `ENegate e2' -> go env e1' e2'
+    | `EAdd (l1, r1), `EAdd (l2, r2) -> go env l1 l2 && go env r1 r2
+    | `ELet (x, xe, be1), `ELet (y, ye, be2) ->
+      let env' = (x, y) :: env in
+      go env xe ye && go env' be1 be2
+    | _ -> false
+  in
+  go [] e1 e2
--- tests/passes/uniquify_tests.ml
+++ tests/passes/uniquify_tests.ml
@@ -1,25 +1,6 @@
 open OUnit2
 open Passes.Uniquify
 
-let alpha_equiv e1 e2 : bool =
-  let rec go env e1 e2 =
-    match (e1, e2) with
-    | `EInt n, `EInt m -> n = m
-    | `ERead, `ERead -> true
-    | `EVar x, `EVar y -> (
-      let y_opt = List.assoc_opt x env in
-      match y_opt with
-      | Some y' -> y = y'
-      | None -> x = y)
-    | `ENegate e1', `ENegate e2' -> go env e1' e2'
-    | `EAdd (l1, r1), `EAdd (l2, r2) -> go env l1 l2 && go env r1 r2
-    | `ELet (x, xe, be1), `ELet (y, ye, be2) ->
-      let env' = (x, y) :: env in
-      go env xe ye && go env' be1 be2
-    | _ -> false
-  in
-  go [] e1 e2
-
 let u1 = `ELet ("a", `ERead, `EVar "a")
 
 let u1' = `ELet ("x0", `ERead, `EVar "x0")
@@ -43,10 +24,10 @@ let u4' =
     , `ELet ("x1", `EInt 8, `ELet ("x2", `EAdd (`EInt 1, `EVar "x1"), `EAdd (`EVar "x2", `EVar "x2"))) )
 
 let test_uniquify _ctxt =
-  assert_equal u1' (uniquify u1) ~cmp:alpha_equiv ~msg:"u1" ~printer:R1.Pp.pp;
-  assert_equal u2' (uniquify u2) ~cmp:alpha_equiv ~msg:"u2" ~printer:R1.Pp.pp;
-  assert_equal u3' (uniquify u3) ~cmp:alpha_equiv ~msg:"u3" ~printer:R1.Pp.pp;
-  assert_equal u4' (uniquify u4) ~cmp:alpha_equiv ~msg:"u4" ~printer:R1.Pp.pp
+  assert_equal u1' (uniquify u1) ~cmp:TestUtils.alpha_equiv ~msg:"u1" ~printer:R1.Pp.pp;
+  assert_equal u2' (uniquify u2) ~cmp:TestUtils.alpha_equiv ~msg:"u2" ~printer:R1.Pp.pp;
+  assert_equal u3' (uniquify u3) ~cmp:TestUtils.alpha_equiv ~msg:"u3" ~printer:R1.Pp.pp;
+  assert_equal u4' (uniquify u4) ~cmp:TestUtils.alpha_equiv ~msg:"u4" ~printer:R1.Pp.pp
 
 let suite = "passes_tests" >::: [ "uniquify" >:: test_uniquify ]
 
