bd42b4ef4e51983a6468bb5a0da07829de89b324
hasantouma <hasantouma@gmail.com>
2021-10-25 13:38:50 -0400

    R1 resolve_complex: Added test case from book

--- tests/passes/resolve_complex_tests.ml
+++ tests/passes/resolve_complex_tests.ml
@@ -29,13 +29,18 @@ let rco6' =
     , `ELet ("x1", `ERead, `ELet ("x2", `ENegate (`EInt 42), `EAdd (`EVar "x1", `EVar "x2")))
     , `ENegate (`EVar "x0") )
 
+let rco7 = `ELet ("x", `EAdd (`EInt 42, `ENegate (`EInt 10)), `EAdd (`EVar "x", `EInt 10))
+
+let rco7' = `ELet ("x", `ELet ("x0", `ENegate (`EInt 10), `EAdd (`EInt 42, `EVar "x0")), `EAdd (`EVar "x", `EInt 10))
+
 let test_resolve_complex _ctxt =
   assert_equal rco1' (resolve_complex rco1) ~cmp:TestUtils.alpha_equiv ~msg:"rco1" ~printer:R1.Pp.pp;
   assert_equal rco2' (resolve_complex rco2) ~cmp:TestUtils.alpha_equiv ~msg:"rco2" ~printer:R1.Pp.pp;
   assert_equal rco3' (resolve_complex rco3) ~cmp:TestUtils.alpha_equiv ~msg:"rco3" ~printer:R1.Pp.pp;
   assert_equal rco4' (resolve_complex rco4) ~cmp:TestUtils.alpha_equiv ~msg:"rco4" ~printer:R1.Pp.pp;
   assert_equal rco5' (resolve_complex rco5) ~cmp:TestUtils.alpha_equiv ~msg:"rco5" ~printer:R1.Pp.pp;
-  assert_equal rco6' (resolve_complex rco6) ~cmp:TestUtils.alpha_equiv ~msg:"rco6" ~printer:R1.Pp.pp
+  assert_equal rco6' (resolve_complex rco6) ~cmp:TestUtils.alpha_equiv ~msg:"rco6" ~printer:R1.Pp.pp;
+  assert_equal rco7' (resolve_complex rco7) ~cmp:TestUtils.alpha_equiv ~msg:"rco7" ~printer:R1.Pp.pp
 
 let suite = "resolve_complex_tests" >::: [ "resolve_complex" >:: test_resolve_complex ]
 
