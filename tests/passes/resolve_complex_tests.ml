ae3f56490c281b853a577af298727b4b574d1e60
hasantouma <hasantouma@gmail.com>
2021-10-25 20:36:27 -0400

    R1 resolve_complex: corner case bug?

--- tests/passes/resolve_complex_tests.ml
+++ tests/passes/resolve_complex_tests.ml
@@ -33,6 +33,10 @@ let rco7 = `ELet ("x", `EAdd (`EInt 42, `ENegate (`EInt 10)), `EAdd (`EVar "x", 
 
 let rco7' = `ELet ("x", `ELet ("x0", `ENegate (`EInt 10), `EAdd (`EInt 42, `EVar "x0")), `EAdd (`EVar "x", `EInt 10))
 
+let rco8 = `ELet ("y", `ELet ("x0", `EInt 20, `EAdd (`EVar "x0", `ELet ("x1", `EInt 22, `EVar "x1"))), `EVar "y")
+
+let rco8' = `ELet ("y", `ELet ("x0", `EInt 20, `ELet ("x1", `EInt 22, `EAdd (`EVar "x0", `EVar "x1"))), `EVar "y")
+
 let test_resolve_complex _ctxt =
   assert_equal rco1' (resolve_complex rco1) ~cmp:TestUtils.alpha_equiv ~msg:"rco1" ~printer:R1.Pp.pp;
   assert_equal rco2' (resolve_complex rco2) ~cmp:TestUtils.alpha_equiv ~msg:"rco2" ~printer:R1.Pp.pp;
@@ -40,7 +44,10 @@ let test_resolve_complex _ctxt =
   assert_equal rco4' (resolve_complex rco4) ~cmp:TestUtils.alpha_equiv ~msg:"rco4" ~printer:R1.Pp.pp;
   assert_equal rco5' (resolve_complex rco5) ~cmp:TestUtils.alpha_equiv ~msg:"rco5" ~printer:R1.Pp.pp;
   assert_equal rco6' (resolve_complex rco6) ~cmp:TestUtils.alpha_equiv ~msg:"rco6" ~printer:R1.Pp.pp;
-  assert_equal rco7' (resolve_complex rco7) ~cmp:TestUtils.alpha_equiv ~msg:"rco7" ~printer:R1.Pp.pp
+  assert_equal rco7' (resolve_complex rco7) ~cmp:TestUtils.alpha_equiv ~msg:"rco7" ~printer:R1.Pp.pp;
+  assert_equal rco8' (resolve_complex rco8) ~cmp:TestUtils.alpha_equiv
+    ~msg:("rco8\n" ^ R1.Pp.pp rco8 ^ "\n")
+    ~printer:(fun x -> "\n" ^ R1.Pp.pp x ^ "\n")
 
 let suite = "resolve_complex_tests" >::: [ "resolve_complex" >:: test_resolve_complex ]
 
