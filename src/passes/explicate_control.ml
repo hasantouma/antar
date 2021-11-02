494a0bb11a1567566798a65c11f42f86763516d6
hasantouma <hasantouma@gmail.com>
2021-11-02 05:18:58 -0400

    Completed 'Task #34 - Implement the explicate-control pass for R1 programs'

--- src/passes/explicate_control.ml
+++ src/passes/explicate_control.ml
@@ -1,6 +1,64 @@
 open C0.Ast
 open R1.Ast
 
-let explicate_control (expr : expr) : p =
+let exp_to_arg (exp : exp) : arg =
+  match exp with
+  | Arg x -> x
+  | _ -> raise (Failure "explicate_control: Invalid exp to arg")
+
+let rec rlang_to_clang (expr : expr) : exp =
   match expr with
-  | _ -> { info = false; blks = [] }
+  | `EInt n -> Arg (Number n)
+  | `EVar x -> Arg (Var x)
+  | `ERead -> Read
+  | `ENegate e ->
+    let e' = rlang_to_clang e in
+    let e'' = exp_to_arg e' in
+    Negate e''
+  | `EAdd (l, r) ->
+    let l' = rlang_to_clang l in
+    let r' = rlang_to_clang r in
+    let l'' = exp_to_arg l' in
+    let r'' = exp_to_arg r' in
+    Add (l'', r'')
+  | _ -> raise (Failure "explicate_control: Invalid rlang to clang")
+
+let rec lift (expr : expr) : (var * expr) list * expr =
+  match expr with
+  | `EInt n -> ([], `EInt n)
+  | `EVar x -> ([], `EVar x)
+  | `ERead -> ([], `ERead)
+  | `ENegate e -> ([], `ENegate e)
+  | `EAdd (l, r) -> ([], `EAdd (l, r))
+  | `ELet (x, ex, eb) ->
+    let xs, ex' = lift ex in
+    let bs, eb' = lift eb in
+    (xs @ [ (x, ex') ] @ bs, eb')
+
+let wrap_ret_arg (expr : expr) : tail =
+  match expr with
+  | `EInt n -> Return (Number n)
+  | `EVar x -> Return (Var x)
+  | `ERead ->
+    let x = Utils.Fresh.fresh_var () in
+    Seq (Set (x, Read), Return (Var x))
+  | `ENegate e ->
+    let x = Utils.Fresh.fresh_var () in
+    let e' = rlang_to_clang e |> exp_to_arg in
+    Seq (Set (x, Negate e'), Return (Var x))
+  | `EAdd (l, r) ->
+    let x = Utils.Fresh.fresh_var () in
+    let l' = rlang_to_clang l |> exp_to_arg in
+    let r' = rlang_to_clang r |> exp_to_arg in
+    Seq (Set (x, Add (l', r')), Return (Var x))
+  | _ -> raise (Failure "explicate_control: Invalid wrap_ret_arg")
+
+let make_clang ((lst, expr) : (var * expr) list * expr) : (label * tail) list =
+  let ret_arg = wrap_ret_arg expr in
+  let lst' = List.map (fun (v, expr) -> (v, rlang_to_clang expr)) lst in
+  let tail = List.fold_right (fun (v, exp) acc -> Seq (Set (v, exp), acc)) lst' ret_arg in
+  [ ("entry", tail) ]
+
+let explicate_control (expr : expr) : p =
+  let blocks = lift expr |> make_clang in
+  { info = false; blks = blocks }
