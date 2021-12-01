c07b4ee2754b9c31b74cd10537dc962922b500d3
hasantouma <hasantouma@gmail.com>
2021-12-01 00:25:20 -0500

    Added tests for `is_uniquify` and `is_resolve_complex`
    Moved `is_uniquify` function to be consistent with other passes ordering

--- tests/passes/resolve_complex_tests.ml
+++ tests/passes/resolve_complex_tests.ml
@@ -34,14 +34,32 @@ let rco7 = `ELet ("x", `EAdd (`EInt 42, `ENegate (`EInt 10)), `EAdd (`EVar "x", 
 let rco7' = `ELet ("x", `ELet ("x0", `ENegate (`EInt 10), `EAdd (`EInt 42, `EVar "x0")), `EAdd (`EVar "x", `EInt 10))
 
 let test_resolve_complex _ctxt =
-  assert_equal rco1' (resolve_complex rco1) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"rco1" ~printer:R1.Pp.pp;
-  assert_equal rco2' (resolve_complex rco2) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"rco2" ~printer:R1.Pp.pp;
-  assert_equal rco3' (resolve_complex rco3) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"rco3" ~printer:R1.Pp.pp;
-  assert_equal rco4' (resolve_complex rco4) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"rco4" ~printer:R1.Pp.pp;
-  assert_equal rco5' (resolve_complex rco5) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"rco5" ~printer:R1.Pp.pp;
-  assert_equal rco6' (resolve_complex rco6) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"rco6" ~printer:R1.Pp.pp;
-  assert_equal rco7' (resolve_complex rco7) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"rco7" ~printer:R1.Pp.pp
+  assert_equal rco1' (resolve_complex rco1) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"resolve_complex: rco1"
+    ~printer:R1.Pp.pp;
+  assert_equal rco2' (resolve_complex rco2) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"resolve_complex: rco2"
+    ~printer:R1.Pp.pp;
+  assert_equal rco3' (resolve_complex rco3) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"resolve_complex: rco3"
+    ~printer:R1.Pp.pp;
+  assert_equal rco4' (resolve_complex rco4) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"resolve_complex: rco4"
+    ~printer:R1.Pp.pp;
+  assert_equal rco5' (resolve_complex rco5) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"resolve_complex: rco5"
+    ~printer:R1.Pp.pp;
+  assert_equal rco6' (resolve_complex rco6) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"resolve_complex: rco6"
+    ~printer:R1.Pp.pp;
+  assert_equal rco7' (resolve_complex rco7) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"resolve_complex: rco7"
+    ~printer:R1.Pp.pp
 
-let suite = "resolve_complex_tests" >::: [ "resolve_complex" >:: test_resolve_complex ]
+let test_is_resolve_complex _ctxt =
+  assert_equal true (is_resolve_complex rco1') ~msg:"is_resolve_complex: rco1" ~printer:string_of_bool;
+  assert_equal true (is_resolve_complex rco2') ~msg:"is_resolve_complex: rco2" ~printer:string_of_bool;
+  assert_equal true (is_resolve_complex rco3') ~msg:"is_resolve_complex: rco3" ~printer:string_of_bool;
+  assert_equal true (is_resolve_complex rco4') ~msg:"is_resolve_complex: rco4" ~printer:string_of_bool;
+  assert_equal true (is_resolve_complex rco5') ~msg:"is_resolve_complex: rco5" ~printer:string_of_bool;
+  assert_equal true (is_resolve_complex rco6') ~msg:"is_resolve_complex: rco6" ~printer:string_of_bool;
+  assert_equal true (is_resolve_complex rco7') ~msg:"is_resolve_complex: rco7" ~printer:string_of_bool
+
+let suite =
+  "resolve_complex_tests"
+  >::: [ "resolve_complex" >:: test_resolve_complex; "resolve_is_complex" >:: test_is_resolve_complex ]
 
 let _ = run_test_tt_main suite
--- tests/passes/uniquify_tests.ml
+++ tests/passes/uniquify_tests.ml
@@ -24,11 +24,17 @@ let u4' =
     , `ELet ("x1", `EInt 8, `ELet ("x2", `EAdd (`EInt 1, `EVar "x1"), `EAdd (`EVar "x2", `EVar "x2"))) )
 
 let test_uniquify _ctxt =
-  assert_equal u1' (uniquify u1) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"u1" ~printer:R1.Pp.pp;
-  assert_equal u2' (uniquify u2) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"u2" ~printer:R1.Pp.pp;
-  assert_equal u3' (uniquify u3) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"u3" ~printer:R1.Pp.pp;
-  assert_equal u4' (uniquify u4) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"u4" ~printer:R1.Pp.pp
+  assert_equal u1' (uniquify u1) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"uniquify: u1" ~printer:R1.Pp.pp;
+  assert_equal u2' (uniquify u2) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"uniquify: u2" ~printer:R1.Pp.pp;
+  assert_equal u3' (uniquify u3) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"uniquify: u3" ~printer:R1.Pp.pp;
+  assert_equal u4' (uniquify u4) ~cmp:TestUtils.rlang_alpha_equiv ~msg:"uniquify: u4" ~printer:R1.Pp.pp
 
-let suite = "passes_tests" >::: [ "uniquify" >:: test_uniquify ]
+let test_is_uniquify _ctxt =
+  assert_equal true (is_uniquify u1') ~msg:"is_uniquify: u1" ~printer:string_of_bool;
+  assert_equal true (is_uniquify u2') ~msg:"is_uniquify: u2" ~printer:string_of_bool;
+  assert_equal true (is_uniquify u3') ~msg:"is_uniquify: u3" ~printer:string_of_bool;
+  assert_equal true (is_uniquify u4') ~msg:"is_uniquify: u4" ~printer:string_of_bool
+
+let suite = "passes_tests" >::: [ "uniquify" >:: test_uniquify; "is_uniquify" >:: test_is_uniquify ]
 
 let _ = run_test_tt_main suite
