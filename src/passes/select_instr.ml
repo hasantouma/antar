babbec9d7703d12943d8d880208dad1b7f1795ae
hasantouma <hasantouma@gmail.com>
2021-12-02 02:25:15 -0500

    Completed 'Task #37 - Implement the uncover-locals pass for C0 programs.'

--- src/passes/select_instr.ml
+++ src/passes/select_instr.ml
@@ -1,3 +1,17 @@
 open C0.Ast
 
-let uncover_locals (p : C0.Ast.p) : C0.Ast.p = p
+let var_of_stmt (s : stmt) : var =
+  match s with
+  | Set (x, _) -> x
+
+let rec get_vars (acc : var list) (tail : tail) : var list =
+  match tail with
+  | Return _ -> acc
+  | Seq (s, t) ->
+    let x = var_of_stmt s in
+    get_vars (x :: acc) t
+
+let uncover_locals (p : C0.Ast.p) : C0.Ast.p =
+  let tail = List.assoc "entry" p.blks in
+  let info' = get_vars [] tail in
+  { p with info = info' }
