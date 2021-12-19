0599e8686a8761ab8e51aad7dd965b49dd219a90
hasantouma <hasantouma@gmail.com>
2021-12-19 01:56:57 -0500

    Moving r0 pp to r1 pp

--- src/rlang/r1/pp.ml
+++ src/rlang/r1/pp.ml
@@ -1,5 +1,14 @@
 let pp_open f indent expr =
   match expr with
+  | `EInt n -> string_of_int n
+  | `ERead -> "(read)"
+  | `ENegate e -> "(- " ^ f indent e ^ ")"
+  | `EAdd (l, r) ->
+    let indent' = indent + 3 in
+    let left = f indent' l in
+    let spaces = String.make indent' ' ' in
+    let right = f indent' r in
+    "(+ " ^ left ^ "\n" ^ spaces ^ right ^ ")"
   | `EVar v -> v
   | `ELet (x, ex, eb) ->
     let indent' = indent + 5 in
@@ -7,7 +16,6 @@ let pp_open f indent expr =
     let body_expr = f indent' eb in
     let spaces = String.make indent' ' ' in
     "(let ([" ^ x ^ " " ^ variable_expr ^ "])\n" ^ spaces ^ body_expr ^ ")"
-  | #R0.Ast.expr_open as expr -> R0.Pp.pp_open f indent expr
 
 let pp expr =
   let rec pp indent expr = pp_open pp indent expr in
