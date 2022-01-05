0c04ab71c2b8cea98895196b79706e9f4a113bca
hasantouma <hasantouma@gmail.com>
2022-01-05 01:26:21 -0500

    Added `anon_func()` to take `input_files` and compile them
    Added '-o' flag for output files, but didn't connect it yet

--- bin/antar.ml
+++ bin/antar.ml
@@ -2,16 +2,25 @@ let lang_name = Printf.sprintf "Antar %s" Rlang.name
 let usage_msg = Printf.sprintf "'%s' programming language" lang_name
 let greetings = Printf.sprintf "Welcome to the '%s' REPL" lang_name
 
+(* hcc helpers *)
+let input_files = ref []
+let output_file = ref ""
+let anon_fun filename = input_files := filename :: !input_files
+
 let parse_cmd_line_args () =
   let speclist =
     [ ("-f", Arg.String Repl.interp_file, "<file_path> Parsing file")
     ; ("-g", Arg.Int (Repl.randp false), "<int> Generate random program of size n")
     ; ("-gv", Arg.Int (Repl.randp true), "<int> Generate, and visualize, random program of size n")
     ; ("-v", Arg.String Repl.visualize, "<file_path> File to visualize")
-    ; ("-c", Arg.String Repl.compile, "<file_path> Output the X86 assembly code")
+    ; ("-c", Arg.String Repl.compile, "<file_path> Output the X86 assembly code") (* TODO: Remove when done *)
+    ; ("-o", Arg.Set_string output_file, "Set output file name")
     ]
   in
-  Arg.parse speclist print_endline usage_msg
+  Arg.parse speclist anon_fun usage_msg;
+  print_endline ("Inputs: " ^ [%show: string list] !input_files);
+  print_endline ("Output: " ^ !output_file);
+  if List.length !input_files > 0 then Repl.compile (List.hd !input_files) else ()
 
 let run () =
   let args_len = Array.length Sys.argv in
